56f319920ca9d0e9e468d80f40842b2c94689ef4
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-10-18 07:32:53 +0300

    update json.erl

--- tasks/basic/src/json.erl
+++ tasks/basic/src/json.erl
@@ -6,14 +6,19 @@ new([{Key, ValueSpec}|Tail]) ->
   Obj = #{Key => createValue(ValueSpec, #{})},
   createValue(Tail, Obj).
 
+createValue([], #{}) -> [];
 createValue([], Obj) -> Obj;
 createValue([{Key, ValueSpec}|Tail], Obj) ->
   NewObj = maps:put(Key, createValue(ValueSpec, #{}), Obj),
   createValue(Tail, NewObj);
 createValue({Key, ValueSpec}, _) ->
   #{Key => createValue(ValueSpec, #{})};
-createValue([_|_] = Arr, _) -> Arr;
+createValue([_|_] = Arr, _) -> createArray(Arr, []);
 createValue(BasicValue, _) -> BasicValue.
+
+createArray([], Acc) -> Acc;
+createArray([Head|Tail], Acc) ->
+  createArray(Tail, Acc ++ [createValue(Head, #{})]).
 
 read(KeySpec, JsonObj) ->
   case maps:is_key(KeySpec, JsonObj) of
